326eb1c3cebe64e8745f4527f1fbc66fbc792380
ngkao <ng.kao.ca@gmail.com>
2023-08-26 23:49:42 -0400

    add profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,21 @@ import FlexBetween from 'components/FlexBetween';
 import { useDispatch } from 'react-redux';
 import { setMode } from 'state';
 import profileImage from "assets/profile.png"
-import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
+import { AppBar, IconButton, InputBase, MenuItem, Toolbar, useTheme, Button, Box, Typography, Menu } from '@mui/material';
 
 const Navbar = ({
     isSidebarOpen,
-    setIsSidebarOpen
+    setIsSidebarOpen,
+    user
 }) => {
     const dispatch = useDispatch();
     const theme = useTheme();
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const isOpen = Boolean(anchorEl);
+    const handleClick = (event) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+
     return (
     <AppBar
         sx={{
@@ -52,6 +59,49 @@ const Navbar = ({
                 <IconButton>
                     <SettingsOutlined sx={{ fontSize: "25px" }} />
                 </IconButton>
+
+                <FlexBetween>
+                    <Button 
+                        onClick={handleClick} 
+                        sx={{ 
+                            display: "flex", 
+                            justifyContent: "space-between", 
+                            alignItems: "center", 
+                            textTransform: "none", 
+                            gap: "1rem",
+                        }}
+                    >
+                        <Box
+                            component="img"
+                            alt="profile"
+                            src={profileImage}
+                            heigh="32px"
+                            width="32px"
+                            borderRadius="50%"
+                            sx={{ objectFit: "cover" }}
+                        />
+                        <Box textAlign="left">
+                            <Typography fontWeight="bold" fontSize="0.85rem" sx={{ color: theme.palette.secondary[100] }}>
+                                {user.name}
+                            </Typography>
+                            <Typography fontSize="0.75rem" sx={{ color: theme.palette.secondary[200] }}>
+                                {user.occupation}
+                            </Typography>
+                        </Box>
+                        <ArrowDropDownOutlined 
+                            sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+                        />
+                    </Button>
+                    <Menu 
+                        anchorEl={anchorEl} 
+                        open={isOpen} 
+                        onClose={handleClose} 
+                        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                    >
+                        <MenuItem onClick={handleClose}>Log Out</MenuItem>
+                    </Menu>
+                </FlexBetween>
+
             </FlexBetween>
 
         </Toolbar>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -96,7 +96,8 @@ const Sidebar = (
  {drawerWidth,
     isSidebarOpen,
     setIsSidebarOpen,
-    isNonMobile
+    isNonMobile,
+    user
 }) => {
     const { pathname } = useLocation();
     const [active, setActive] = useState("");
@@ -131,7 +132,7 @@ const Sidebar = (
                             <FlexBetween color={theme.palette.secondary.main}>
                                 <Box display="flex" alignItems="center" gap="0.5rem">
                                     <Typography variant="h4" fontWeight="bold">
-                                        NgDash
+                                        DASHBOARD
                                     </Typography>
                                 </Box>
                                 {!isNonMobile && (
@@ -145,7 +146,7 @@ const Sidebar = (
                             {navItems.map(({text, icon}) => {
                                 if (!icon) {
                                     return (
-                                        <Typography key={text} sx={{m: "2.25rem 0 1rem 3rem"}}>
+                                        <Typography key={text} sx={{m: "0.5rem 0 0.5rem 3rem"}}>
                                             {text}
                                         </Typography>
                                     )
@@ -160,7 +161,8 @@ const Sidebar = (
                                             }}
                                             sx={{
                                                 backgroundColor: active === lcText ? theme.palette.secondary[300] : "transparent",
-                                                color: active === lcText ? theme.palette.primary[600] : theme.palette.secondary[100]
+                                                color: active === lcText ? theme.palette.primary[600] : theme.palette.secondary[100],
+                                                padding: "4px"
                                             }}
                                         >
                                             <ListItemIcon
@@ -180,6 +182,32 @@ const Sidebar = (
                                 );
                             })}
                         </List>
+                    </Box>
+
+                    <Box position="absolute" bottom="2rem">
+                        <Divider />
+                        <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+                            <Box
+                                component="img"
+                                alt="profile"
+                                src={profileImage}
+                                heigh="40px"
+                                width="40px"
+                                borderRadius="50%"
+                                sx={{ objectFit: "cover" }}
+                            />
+                                <Box textAlign="left">
+                                    <Typography fontWeight="bold" fontSize="0.9rem" sx={{ color: theme.palette.secondary[100] }}>
+                                        {user.name}
+                                    </Typography>
+                                    <Typography fontSize="0.8rem" sx={{ color: theme.palette.secondary[200] }}>
+                                        {user.occupation}
+                                    </Typography>
+                                </Box>
+                                <SettingsOutlined
+                                    sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+                                />
+                        </FlexBetween>
                     </Box>
                 </Drawer>
             )}
